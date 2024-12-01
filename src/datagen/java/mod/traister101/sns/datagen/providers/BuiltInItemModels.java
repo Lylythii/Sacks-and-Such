e5da0b95cbd80fdc03bca8416de0815cfac75839
Traister101 <66151907+Traister101@users.noreply.github.com>
2024-12-01 14:09:11 -0800

    Silly unneeded revamp of some model builder method overloads

--- src/datagen/java/mod/traister101/sns/datagen/providers/BuiltInItemModels.java
+++ src/datagen/java/mod/traister101/sns/datagen/providers/BuiltInItemModels.java
@@ -1,7 +1,7 @@
 package mod.traister101.sns.datagen.providers;
 
 import com.google.common.base.Preconditions;
-import com.google.errorprone.annotations.CanIgnoreReturnValue;
+import com.google.errorprone.annotations.*;
 import mod.traister101.sns.SacksNSuch;
 import mod.traister101.sns.common.items.SNSItems;
 
@@ -15,6 +15,7 @@ import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.registries.ForgeRegistries;
 
 import java.util.Objects;
+import java.util.function.Supplier;
 
 public class BuiltInItemModels extends ItemModelProvider {
 
@@ -27,27 +28,32 @@ public class BuiltInItemModels extends ItemModelProvider {
 
 	@Override
 	protected void registerModels() {
-		basicItem(SNSItems.UNFINISHED_LEATHER_SACK.get());
-		basicItem(SNSItems.REINFORCED_FIBER.get());
-		basicItem(SNSItems.REINFORCED_FABRIC.get());
-		basicItem(SNSItems.PACK_FRAME.get());
-		basicItem(SNSItems.HIKING_BOOTS.get());
-		basicItem(SNSItems.STEEL_TOE_HIKING_BOOTS.get(), new ResourceLocation(SacksNSuch.MODID, "item/hiking_boots"));
-		basicItem(SNSItems.BLACK_STEEL_TOE_HIKING_BOOTS.get(), new ResourceLocation(SacksNSuch.MODID, "item/hiking_boots"));
-		basicItem(SNSItems.BLUE_STEEL_TOE_HIKING_BOOTS.get(), new ResourceLocation(SacksNSuch.MODID, "item/hiking_boots"));
-		basicItem(SNSItems.RED_STEEL_TOE_HIKING_BOOTS.get(), new ResourceLocation(SacksNSuch.MODID, "item/hiking_boots"));
-		basicItem(SNSItems.QUIVER.get());
+		basicItem(SNSItems.UNFINISHED_LEATHER_SACK);
+		basicItem(SNSItems.REINFORCED_FIBER);
+		basicItem(SNSItems.REINFORCED_FABRIC);
+		basicItem(SNSItems.PACK_FRAME);
+		basicItem(SNSItems.LEATHER_STRIP);
+		basicItem(SNSItems.BOUND_LEATHER_STRIP);
+		basicItem(SNSItems.LEATHER_STRIP);
+		basicItem(SNSItems.BOUND_LEATHER_STRIP);
+		basicItem(SNSItems.HIKING_BOOTS);
+		basicItem(SNSItems.STEEL_TOE_HIKING_BOOTS, new ResourceLocation(SacksNSuch.MODID, "item/hiking_boots"));
+		basicItem(SNSItems.BLACK_STEEL_TOE_HIKING_BOOTS, new ResourceLocation(SacksNSuch.MODID, "item/hiking_boots"));
+		basicItem(SNSItems.BLUE_STEEL_TOE_HIKING_BOOTS, new ResourceLocation(SacksNSuch.MODID, "item/hiking_boots"));
+		basicItem(SNSItems.RED_STEEL_TOE_HIKING_BOOTS, new ResourceLocation(SacksNSuch.MODID, "item/hiking_boots"));
+		basicItem(SNSItems.QUIVER);
+		basicItem(SNSItems.STEEL_HORSESHOE);
+		basicItem(SNSItems.STEEL_HORSESHOES);
 
-		iconWithHeldModel(SNSItems.STRAW_BASKET.get());
-		iconWithHeldModel(SNSItems.LEATHER_SACK.get(),
+		iconWithHeldModel(SNSItems.STRAW_BASKET);
+		iconWithHeldModel(SNSItems.LEATHER_SACK,
 				withExistingParent("item/held/leather_sack", SMALL_SACK).texture("sack", modLoc("item/held/leather_sack")));
-		iconWithHeldModel(SNSItems.BURLAP_SACK.get(),
+		iconWithHeldModel(SNSItems.BURLAP_SACK,
 				withExistingParent("item/held/burlap_sack", SMALL_SACK).texture("sack", modLoc("item/held/burlap_sack")));
-		iconWithHeldModel(SNSItems.ORE_SACK.get(),
-				withExistingParent("item/held/ore_sack", LARGE_SACK).texture("sack", modLoc("item/held/ore_sack")));
-		iconWithHeldModel(SNSItems.SEED_POUCH.get(),
+		iconWithHeldModel(SNSItems.ORE_SACK, withExistingParent("item/held/ore_sack", LARGE_SACK).texture("sack", modLoc("item/held/ore_sack")));
+		iconWithHeldModel(SNSItems.SEED_POUCH,
 				withExistingParent("item/held/seed_pouch", SMALL_SACK).texture("sack", modLoc("item/held/seed_pouch")));
-		iconWithHeldModel(SNSItems.FRAME_PACK.get());
+		iconWithHeldModel(SNSItems.FRAME_PACK);
 	}
 
 	@Override
@@ -56,6 +62,16 @@ public class BuiltInItemModels extends ItemModelProvider {
 		final ResourceLocation outputLoc = extendWithFolder(path.contains(":") ? mcLoc(path) : modLoc(path));
 		existingFileHelper.trackGenerated(outputLoc, MODEL);
 		return generatedModels.computeIfAbsent(outputLoc, factory);
+	}
+
+	@CanIgnoreReturnValue
+	public ItemModelBuilder basicItem(final Supplier<? extends Item> item) {
+		return basicItem(item.get());
+	}
+
+	@CanIgnoreReturnValue
+	public ItemModelBuilder basicItem(final Supplier<? extends Item> item, final ResourceLocation texture) {
+		return basicItem(item.get(), texture);
 	}
 
 	private ResourceLocation extendWithFolder(final ResourceLocation rl) {
@@ -68,29 +84,42 @@ public class BuiltInItemModels extends ItemModelProvider {
 		return basicItem(Objects.requireNonNull(ForgeRegistries.ITEMS.getKey(item)), texture);
 	}
 
+	@CanIgnoreReturnValue
 	public ItemModelBuilder basicItem(final ResourceLocation item, final ResourceLocation texture) {
 		return getBuilder(item.toString()).parent(new ModelFile.UncheckedModelFile("item/generated")).texture("layer0", texture);
 	}
 
+	@CanIgnoreReturnValue
+	private SeparateTransformsModelBuilder<ItemModelBuilder> iconWithHeldModel(final Supplier<? extends Item> item) {
+		return iconWithHeldModel(item.get());
+	}
 
 	@SuppressWarnings("UnusedReturnValue")
 	private SeparateTransformsModelBuilder<ItemModelBuilder> iconWithHeldModel(final Item item) {
 		return iconWithHeldModel(Objects.requireNonNull(ForgeRegistries.ITEMS.getKey(item)));
 	}
 
+	@CanIgnoreReturnValue
 	private SeparateTransformsModelBuilder<ItemModelBuilder> iconWithHeldModel(final ResourceLocation item) {
 		return iconWithHeldModel(item, getExistingFile(item.withPrefix(ITEM_FOLDER + "/held/")));
 	}
 
-	@SuppressWarnings("UnusedReturnValue")
+	@CanIgnoreReturnValue
+	private SeparateTransformsModelBuilder<ItemModelBuilder> iconWithHeldModel(final Supplier<? extends Item> item, final ModelFile heldModel) {
+		return iconWithHeldModel(item.get(), heldModel);
+	}
+
+	@CanIgnoreReturnValue
 	private SeparateTransformsModelBuilder<ItemModelBuilder> iconWithHeldModel(final Item item, final ModelFile heldModel) {
 		return iconWithHeldModel(Objects.requireNonNull(ForgeRegistries.ITEMS.getKey(item)), heldModel);
 	}
 
+	@CanIgnoreReturnValue
 	private SeparateTransformsModelBuilder<ItemModelBuilder> iconWithHeldModel(final ResourceLocation item, final ModelFile heldModel) {
 		return iconWithHeldModel(item, heldModel, icon(item));
 	}
 
+	@CanIgnoreReturnValue
 	private SeparateTransformsModelBuilder<ItemModelBuilder> iconWithHeldModel(final ResourceLocation item, final ModelFile heldModel,
 			final ModelFile iconModel) {
 		return getTransformedItemModelBuilder(item).base(nested().parent(heldModel))
@@ -99,21 +128,31 @@ public class BuiltInItemModels extends ItemModelProvider {
 				.perspective(ItemDisplayContext.FIXED, nested().parent(iconModel));
 	}
 
+	@CanIgnoreReturnValue
 	@SuppressWarnings("unused")
 	private ItemModelBuilder icon(final Item item) {
 		return icon(Objects.requireNonNull(ForgeRegistries.ITEMS.getKey(item)));
 	}
 
+	@CanIgnoreReturnValue
 	private ItemModelBuilder icon(final ResourceLocation item) {
 		return getBuilder(item.withPrefix("item/icon/").toString()).parent(new ModelFile.UncheckedModelFile("item/generated"))
 				.texture("layer0", item.withPrefix("item/icon/"));
 	}
 
+	@CheckReturnValue
+	@SuppressWarnings("unused")
+	private SeparateTransformsModelBuilder<ItemModelBuilder> getTransformedItemModelBuilder(final Supplier<? extends Item> item) {
+		return getTransformedItemModelBuilder(item.get());
+	}
+
+	@CheckReturnValue
 	@SuppressWarnings("unused")
 	private SeparateTransformsModelBuilder<ItemModelBuilder> getTransformedItemModelBuilder(final Item item) {
 		return getTransformedItemModelBuilder(Objects.requireNonNull(ForgeRegistries.ITEMS.getKey(item)));
 	}
 
+	@CheckReturnValue
 	private SeparateTransformsModelBuilder<ItemModelBuilder> getTransformedItemModelBuilder(final ResourceLocation key) {
 		return getBuilder(key.toString()).parent(getExistingFile(new ResourceLocation("forge", "item/default")))
 				.customLoader(SeparateTransformsModelBuilder::begin);
